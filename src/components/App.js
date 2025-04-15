a5ee4a9a9394fcc19512807938002cdce376de17
Sheila Samita <samitasheila1@gmail.com>
2025-04-15 18:51:18 +0300

    Applied props basics

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,22 @@
 import React from "react";
-import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
-
-// pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
-console.log(user);
 
 function App() {
+  console.log(user);
+
   return (
     <div>
-      <NavBar />
-      <Home />
-      <About />
+      <nav>
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+      </nav>
+      <Home name={user.name} city={user.city} color={user.color} />
+      <About bio={user.bio} links={user.links} />
     </div>
   );
 }
 
 export default App;
+
